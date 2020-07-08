da705b85f610cd3ea7fe8682f3e4e8b99930f104
Shoma Okamoto <32533860+shmokmt@users.noreply.github.com>
2020-07-08 23:24:49 +0900

    アクセス数サンプルプログラム作った

--- main.go
+++ main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+	"sync"
 )
 
-func main() {
-	for i := 0; i < 100; i++ {
-		go func() {
-			_, err := http.Get("http://localhost")
-			if err != nil {
-				log.Fatal(err)
-			}
-		}()
+func request() {
+	resp, err := http.Get("http://localhost")
+	if err != nil {
+		log.Print(err)
+		return
 	}
+	defer resp.Body.Close()
+	result, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	title := "server-2"
+	if strings.Contains(string(result), "server-1") {
+		title = "server-1"
+	}
+	log.Println(title)
+	wg.Done()
+}
 
+var wg sync.WaitGroup
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		wg.Add(1)
+		go request()
+	}
+	wg.Wait()
 }
